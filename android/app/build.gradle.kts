b54e9f429270adb9dc6f56a172b8fb92d00faecb
ShahmeerKhan1 <shahmeer1ssuet@gmail.com>
2025-07-08 16:41:47 +0500

    final commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,11 @@ plugins {
 android {
     namespace = "com.example.bussels"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -37,6 +39,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
